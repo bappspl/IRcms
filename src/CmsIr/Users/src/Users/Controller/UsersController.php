2f7edcb39d703a1173e30dc19bd3c5c61fcf54c7
idik91 <idzikkrzysztof91@gmail.com>
2014-09-23 19:16:25 +0200

    zapis nowego uzytkownika

--- src/CmsIr/Users/src/Users/Controller/UsersController.php
+++ src/CmsIr/Users/src/Users/Controller/UsersController.php
@@ -9,11 +9,14 @@ use Zend\Db\Sql\Predicate;
 use CmsIr\Users\Form\UserForm;
 use CmsIr\Users\Form\UserFormFilter;
 
+use Zend\Mail\Message;
 
 class UsersController extends AbstractActionController
 {
     protected $usersTable;
     protected $uploadDir = 'public/files/users/';
+    protected $appName = 'Cms-ir';
+
     public function usersListAction()
     {
         $request = $this->getRequest();
@@ -55,9 +58,14 @@ class UsersController extends AbstractActionController
             $form->setData($request->getPost());
 
             if ($form->isValid()) {
-                $data = $request->getPost();
+                $data = $form->getData();
+                $data = $this->prepareData($data);
 
-                var_dump($data);die;
+                $user = new Users();
+                $user->exchangeArray($data[0]);
+
+                $this->getUsersTable()->saveUser($user);
+                $this->sendConfirmationEmail($user, $data[1]);
             }
         }
 
@@ -125,6 +133,133 @@ class UsersController extends AbstractActionController
         }
         return $this->response;
     }
+
+    public function generatePassword($l = 8, $c = 0, $n = 0, $s = 0)
+    {
+        // get count of all required minimum special chars
+        $count = $c + $n + $s;
+        $out = '';
+        // sanitize inputs; should be self-explanatory
+        if(!is_int($l) || !is_int($c) || !is_int($n) || !is_int($s)) {
+            trigger_error('Argument(s) not an integer', E_USER_WARNING);
+            return false;
+        }
+        elseif($l < 0 || $l > 20 || $c < 0 || $n < 0 || $s < 0) {
+            trigger_error('Argument(s) out of range', E_USER_WARNING);
+            return false;
+        }
+        elseif($c > $l) {
+            trigger_error('Number of password capitals required exceeds password length', E_USER_WARNING);
+            return false;
+        }
+        elseif($n > $l) {
+            trigger_error('Number of password numerals exceeds password length', E_USER_WARNING);
+            return false;
+        }
+        elseif($s > $l) {
+            trigger_error('Number of password capitals exceeds password length', E_USER_WARNING);
+            return false;
+        }
+        elseif($count > $l) {
+            trigger_error('Number of password special characters exceeds specified password length', E_USER_WARNING);
+            return false;
+        }
+
+        // all inputs clean, proceed to build password
+
+        // change these strings if you want to include or exclude possible password characters
+        $chars = "abcdefghijklmnopqrstuvwxyz";
+        $caps = strtoupper($chars);
+        $nums = "0123456789";
+        $syms = "!@#$%^&*()-+?";
+
+        // build the base password of all lower-case letters
+        for($i = 0; $i < $l; $i++) {
+            $out .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+
+        // create arrays if special character(s) required
+        if($count) {
+            // split base password to array; create special chars array
+            $tmp1 = str_split($out);
+            $tmp2 = array();
+
+            // add required special character(s) to second array
+            for($i = 0; $i < $c; $i++) {
+                array_push($tmp2, substr($caps, mt_rand(0, strlen($caps) - 1), 1));
+            }
+            for($i = 0; $i < $n; $i++) {
+                array_push($tmp2, substr($nums, mt_rand(0, strlen($nums) - 1), 1));
+            }
+            for($i = 0; $i < $s; $i++) {
+                array_push($tmp2, substr($syms, mt_rand(0, strlen($syms) - 1), 1));
+            }
+
+            // hack off a chunk of the base password array that's as big as the special chars array
+            $tmp1 = array_slice($tmp1, 0, $l - $count);
+            // merge special character(s) array with base password array
+            $tmp1 = array_merge($tmp1, $tmp2);
+            // mix the characters up
+            shuffle($tmp1);
+            // convert to string for output
+            $out = implode('', $tmp1);
+        }
+
+        return $out;
+    }
+
+    public function prepareData($data)
+    {
+        $randomPassword = uniqid();
+        $data['active'] = 0;
+        $data['password_salt'] = $this->generateDynamicSalt();
+        $data['password'] = $this->encriptPassword(
+            $this->getStaticSalt(),
+            $randomPassword,
+            $data['password_salt']
+        );
+        $data['registration_date'] = date('Y-m-d H:i:s');
+        $date = new \DateTime();
+        $data['registration_date'] = $date->format('Y-m-d H:i:s');
+        $data['registration_token'] = md5(uniqid(mt_rand(), true));
+        $data['email_confirmed'] = 0;
+        return array($data, $randomPassword);
+    }
+
+    public function encriptPassword($staticSalt, $password, $dynamicSalt)
+    {
+        return $password = md5($staticSalt . $password . $dynamicSalt);
+    }
+
+    public function getStaticSalt()
+    {
+        $staticSalt = '';
+        $config = $this->getServiceLocator()->get('Config');
+        $staticSalt = $config['static_salt'];
+        return $staticSalt;
+    }
+
+    public function generateDynamicSalt()
+    {
+        $dynamicSalt = '';
+        for ($i = 0; $i < 50; $i++) {
+            $dynamicSalt .= chr(rand(35, 126));
+        }
+        return $dynamicSalt;
+    }
+
+    public function sendConfirmationEmail(Users $user, $password)
+    {
+        $transport = $this->getServiceLocator()->get('mail.transport');
+        $message = new Message();
+        $this->getRequest()->getServer();
+        $message->addTo($user->getEmail())
+            ->addFrom('mailer@web-ir.pl')
+            ->setSubject('Rejestracja w serwisie: ' . $this->appName)
+            ->setBody("Utworzono konto dla ciebie w serwisie " . $this->appName . "<br> Twoje hasło to: " .  $password);
+        $transport->send($message);
+    }
+
     /**
      * @return \CmsIr\Users\Model\UsersTable
      */
