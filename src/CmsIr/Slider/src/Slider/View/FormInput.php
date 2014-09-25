4a6bfeaeca654d0244cc1f9d134f5fe29ac86411
idik91 <idzikkrzysztof91@gmail.com>
2014-09-25 20:43:22 +0200

    rozroznienie typow pol formularz i inne ich renderowanie

--- src/CmsIr/Slider/src/Slider/View/FormInput.php
+++ src/CmsIr/Slider/src/Slider/View/FormInput.php
@@ -26,23 +26,52 @@ class FormInput extends ZendFormInput
         $attributes['type']  = $this->getType($element);
         $attributes['value'] = $element->getValue();
 
-        $error = new FormElementErrors();
+        switch ($this->getType($element)) {
+            case 'hidden' :
+                return sprintf(
+                    '<input %s class="form-control" %s  ',
+                    $this->createAttributesString($attributes),
+                    $this->getInlineClosingBracket()
 
-        $errorMessage =  $error->setMessageOpenFormat('<small data-bv-validator="notEmpty" class="help-block" style="color: #E9573F">')
-        ->setMessageCloseString('</small>')
-        ->render($element);
+                );
+            break;
+            case 'file' :
+                return sprintf(
+                    '<input %s class="form-control" %s  ',
+                    $this->createAttributesString($attributes),
+                    $this->getInlineClosingBracket()
 
-        $options = $element->getOptions();
-        $options = reset($options);
+                );
+            break;
+            case 'submit' :
+                return sprintf(
+                    '<input %s class="form-control" %s  ',
+                    $this->createAttributesString($attributes),
+                    $this->getInlineClosingBracket()
 
-        return sprintf(
-            '<div class="form group ' . (count($element->getMessages()) > 0 ? 'has-error' : '') . ' has-feedback">
-            <label> %s </label><input %s class="form-control" %s
-            ' . (count($element->getMessages()) > 0 ? '<span class="glyphicon glyphicon-remove form-control-feedback"></span>' : '') . '</div> %s',
-            $options,
-            $this->createAttributesString($attributes),
-            $this->getInlineClosingBracket(),
-            $errorMessage
-        );
+                );
+            break;
+            default:
+                $error = new FormElementErrors();
+
+                $errorMessage =  $error->setMessageOpenFormat('<small data-bv-validator="notEmpty" class="help-block" style="color: #E9573F">')
+                    ->setMessageCloseString('</small>')
+                    ->render($element);
+
+                $options = $element->getOptions();
+                $options = reset($options);
+
+                return sprintf(
+                    '<div class="form group ' . (count($element->getMessages()) > 0 ? 'has-error' : '') . ' has-feedback">
+                    <label> %s </label><input %s class="form-control" %s
+                    ' . (count($element->getMessages()) > 0 ? '<span class="glyphicon glyphicon-remove form-control-feedback"></span>' : '') . '</div> %s',
+                    $options,
+                    $this->createAttributesString($attributes),
+                    $this->getInlineClosingBracket(),
+                    $errorMessage
+                );
+            break;
+        }
+
     }
 }
